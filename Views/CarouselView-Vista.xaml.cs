dc39f708ff7be1ea79b462b31be2af0e3a315ef2
Ruben Darío Rueda <ingeniero.rubenrueda@gmail.com>
2023-07-05 15:00:16 -0500

    FirstCommit
    Add settings on Layout.

---
+++ Views/CarouselView-Vista.xaml.cs
@@ -0,0 +1,14 @@
+namespace ControlesTipoVista.Views;
+
+public partial class CarouselView_Vista : ContentPage
+{
+	public CarouselView_Vista()
+	{
+		InitializeComponent();
+	}
+
+    private void CarouselView_RemainingItemsThresholdReached(object sender, EventArgs e)
+    {
+		//colocar el codigo a ejecutar
+    }
+}
